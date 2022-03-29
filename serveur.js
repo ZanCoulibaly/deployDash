7375214de989c2931602d71f74c3637b3a896bf5
ZanCoulibaly <58905348+ZanCoulibaly@users.noreply.github.com>
2022-03-29 17:06:38 +0000

    refaire

--- serveur.js
+++ serveur.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require("path");
 
 const app = express();
-app.unsubscribe(express.static(__dirname + '/dist/DASHBOARD-PTD'));
+app.unsubscribe(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/DASHBOARD-PTD/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
 
 app.listen(process.env.PORT);
