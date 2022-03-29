561efdb6400c7f5f50d65920d1818dc79bca5628
ZanCoulibaly <58905348+ZanCoulibaly@users.noreply.github.com>
2022-03-29 17:18:02 +0000

    new

--- serveur.js
+++ serveur.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require("path");
 
 const app = express();
-app.unsubscribe(express.static(__dirname + '/dist'));
+app.unsubscribe(express.static(__dirname + '/dist/dashboard-PTD'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/dashboard-PTD/index.html'));
 })
 
 app.listen(process.env.PORT);
