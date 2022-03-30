40fd0c73f4183bc2cf1a5e695404000dc1a518b8
ZanCoulibaly <58905348+ZanCoulibaly@users.noreply.github.com>
2022-03-30 09:20:44 +0000

    le lien de dist

--- serveur.js
+++ serveur.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require("path");
 
 const app = express();
-app.unsubscribe(express.static(__dirname + '/dist/'));
+app.unsubscribe(express.static(__dirname + '/dist/dashboard'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/dashboard/index.html'));
 })
 
 app.listen(process.env.PORT);
